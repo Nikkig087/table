982a1af496b10c344539f9ccf6589330410a3f2e
NikkiGetsee <Nicola.getsee@gmail.com>
2024-06-21 20:30:06 +0000

    manipulate CSS classes

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+/*the ready function makes sure the dom is loaded before anything inside it runs*/
+
+
+$(document).ready(function() {
+
+    //Use jQuery to change CSS classes
+
+    //$("h2").addClass("underline") // underline all h2 elements
+    //$("ul").addClass("border") // adds a border to all lists
+
+    // we can also do remove class
+    //$("h2").removeClass("underline") // removes underline all h2 elements
+    //$("ul").removeClass("border") // removes a border to all lists
+});
